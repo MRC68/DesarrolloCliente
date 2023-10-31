d282d251dca461d7800758f5ffb5cf7684bca2b1
btudo.ceu <btudo@ceuandalucia.es>
2023-10-31 22:15:58 +0100

    Añado comentarios

--- MramosEjercicio_T2-03/js/codigo.js
+++ MramosEjercicio_T2-03/js/codigo.js
@@ -7,7 +7,8 @@ bicho2 = document.getElementById("t2");
 reinicio = () => {
   bicho1.style.left = 0 + "%";
   bicho2.style.left = 0 + "%";
-  console.log("Reiniciando la carrera");};
+  console.log("Reiniciando la carrera");
+};
 
 // Movimiento
 gogogo = () => {
@@ -15,7 +16,8 @@ gogogo = () => {
   bicho2.style.left = posicion + "%";
   posicion++;
 
-  if (posicion >= 80) {
+  if (posicion >= 90) {
+    //belen. Cambio a 90
     posicion = 0;
   }
 };
@@ -33,17 +35,34 @@ mueveteya = () => {
   bicho1.style.left = posicion + "%";
   bicho2.style.left = posicion + 6 + "%";
   posicion++;
-  if (posicion >= 80) {posicion = 0;}};
+  if (posicion >= 90) {
+    //belen. Cambio a 90
+    posicion = 0;
+  }
+};
 // Intervalo
 let run = () => {
   formu.comenzar.disabled = true;
   formu.empate.disabled = true;
   formu.correr.disabled = true;
   tempo = setInterval(mueveteya, 70);
-  console.log("Iniciando carrera...");};
+  console.log("Iniciando carrera...");
+};
 
 // Detener (?)
 stop = () => {
+  event.preventDefault(); //BELEN . ESTA OPCION SOLO SI ES SUBMIT
   clearInterval(tempo);
   console.log("Parando la carrera...");
 };
+
+/* 
+Bien María,
+Lo que te pasa en la función stop es que estás utlizando un botón de tipo submit. Por defecto, los submit,
+recargan la pagina. Para que no lo haga, tienes dos opciones:
+- o pones type="button"
+- o pones type="button" y se le tiene que indicar que por defecto no recargue: con  preventDefault();
+
+Te he subido a 90, la copmparacion del if para que el tigre llegue más al final de la pantlla. Se quedaba un
+poco corto.
+*/
